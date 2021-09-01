c0f2d232e4d249a2cc706593393ce29eb53ca435
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2021-09-01 13:17:55 +0200

    try initializing the DynamoDb client faster

---
+++ user-access-service/src/main/java/no/unit/nva/database/Constants.java
@@ -0,0 +1,17 @@
+package no.unit.nva.database;
+
+import nva.commons.core.Environment;
+import nva.commons.core.JacocoGenerated;
+
+@JacocoGenerated
+public final class Constants {
+
+    @JacocoGenerated
+    private static final Environment ENVIRONMENT = new Environment();
+    @JacocoGenerated
+    public static final String AWS_REGION = ENVIRONMENT.readEnv("AWS_REGION");
+
+    private Constants() {
+
+    }
+}

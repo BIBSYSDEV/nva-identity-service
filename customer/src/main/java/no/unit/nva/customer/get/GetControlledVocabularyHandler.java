56bfd3f231f9430af8e1a4516764444fd4be1938
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2021-09-30 14:48:21 +0200

    NP-2474: getVocabularies returns result with context

--- customer/src/main/java/no/unit/nva/customer/get/GetControlledVocabularyHandler.java
+++ customer/src/main/java/no/unit/nva/customer/get/GetControlledVocabularyHandler.java
@@ -3,12 +3,14 @@ package no.unit.nva.customer.get;
 import com.amazonaws.services.lambda.runtime.Context;
 import java.net.HttpURLConnection;
 import java.net.URI;
+import java.util.Set;
 import no.unit.nva.customer.model.VocabularySettingDto;
 import no.unit.nva.customer.model.VocabularyStatus;
+import no.unit.nva.customer.model.interfaces.VocabularySettingsList;
 import nva.commons.apigateway.ApiGatewayHandler;
 import nva.commons.apigateway.RequestInfo;
 
-public class GetControlledVocabularyHandler extends ApiGatewayHandler<Void, VocabularySettingDto[]> {
+public class GetControlledVocabularyHandler extends ApiGatewayHandler<Void, VocabularySettingsList> {
 
     public static final VocabularySettingDto HARDCODED_RESPONSE = new VocabularySettingDto(
         "sampleVocabulary",
@@ -20,12 +22,12 @@ public class GetControlledVocabularyHandler extends ApiGatewayHandler<Void, Voca
     }
 
     @Override
-    protected VocabularySettingDto[] processInput(Void input, RequestInfo requestInfo, Context context) {
-        return new VocabularySettingDto[]{HARDCODED_RESPONSE};
+    protected VocabularySettingsList processInput(Void input, RequestInfo requestInfo, Context context) {
+        return new VocabularySettingsList(Set.of(HARDCODED_RESPONSE));
     }
 
     @Override
-    protected Integer getSuccessStatusCode(Void input, VocabularySettingDto[] output) {
+    protected Integer getSuccessStatusCode(Void input, VocabularySettingsList output) {
         return HttpURLConnection.HTTP_OK;
     }
 }

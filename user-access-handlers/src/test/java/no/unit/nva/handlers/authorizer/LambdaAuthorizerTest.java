698f1ee36e982200c8e3932d4614e242466897b7
Jan Bjørndalen <jbj@aztek.no>
2021-10-25 14:12:32 +0200

    Revert "Trying Simple response"
    This reverts commit 1664bf5c2a84df2fe62345a8362da7b3942a6d60.

--- user-access-handlers/src/test/java/no/unit/nva/handlers/authorizer/LambdaAuthorizerTest.java
+++ user-access-handlers/src/test/java/no/unit/nva/handlers/authorizer/LambdaAuthorizerTest.java
@@ -1,29 +1,31 @@
 package no.unit.nva.handlers.authorizer;
 
+import static no.unit.nva.useraccessmanagement.RestConfig.defaultRestObjectMapper;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.secretsmanager.AWSSecretsManager;
 import com.amazonaws.services.secretsmanager.model.GetSecretValueRequest;
 import com.amazonaws.services.secretsmanager.model.GetSecretValueResult;
 import com.amazonaws.services.secretsmanager.model.ResourceNotFoundException;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+import no.unit.commons.apigateway.authentication.AuthorizerResponse;
+import no.unit.commons.apigateway.authentication.StatementElement;
 import no.unit.nva.database.interfaces.WithEnvironment;
 import no.unit.nva.testutils.HandlerRequestBuilder;
 import nva.commons.core.Environment;
 import org.apache.http.HttpHeaders;
 import org.junit.jupiter.api.Test;
 import org.mockito.stubbing.Answer;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Map;
-
-import static no.unit.nva.useraccessmanagement.RestConfig.defaultRestObjectMapper;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class LambdaAuthorizerTest implements WithEnvironment {
 
@@ -32,6 +34,10 @@ public class LambdaAuthorizerTest implements WithEnvironment {
     public static final String CORRECT_SECRET_VALUE = "someSecretValue";
     public static final String AWS_SERCRETS_MANAGER_ERROR_MESSAGE = "AwsSercretsManager message";
     public static final String DEFAULT_ENV_VALUE = "*";
+    public static final int SINGLE_STATEMENT_EXPECTED = 1;
+    public static final int UNIQUE_STATEMENT = 0;
+    public static final String ACTION_ALLOWED = "Allow";
+    public static final String ACTION_DENIED = "Deny";
 
     public static final String DEFAULT_METHOD_ARN = "arn:aws:execute-api:eu-west-1:884807050265:2lcqynkwke/Prod/GET"
             + "/service/users/orestis@unit.no";
@@ -53,9 +59,14 @@ public class LambdaAuthorizerTest implements WithEnvironment {
     @Test
     public void authorizerReturnsAcceptPolicyWhenSecretIsCorrect() throws IOException {
 
-        SimpleAuthorizerResponse response = sendRequest(envWithCorrectValues);
+        AuthorizerResponse response = sendRequest(envWithCorrectValues);
 
-        assertThat(response.getIsAuthorized(), is(true));
+        List<StatementElement> statements = response.getPolicyDocument().getStatement();
+        assertThat(statements.size(), is(equalTo(SINGLE_STATEMENT_EXPECTED)));
+
+        StatementElement statement = statements.get(UNIQUE_STATEMENT);
+        String actualEffect = statement.getEffect();
+        assertThat(actualEffect, is(equalTo(ACTION_ALLOWED)));
     }
 
     @Test
@@ -65,9 +76,14 @@ public class LambdaAuthorizerTest implements WithEnvironment {
                 LambdaAuthorizer.AWS_SECRET_KEY_ENV_VAR, CORRECT_SECRET_KEY);
 
         Environment envWithWrongSecretName = mockEnvironment(wrongSecretName, DEFAULT_ENV_VALUE);
-        SimpleAuthorizerResponse response = sendRequest(envWithWrongSecretName);
+        AuthorizerResponse response = sendRequest(envWithWrongSecretName);
 
-        assertThat(response.getIsAuthorized(), is(false));
+        List<StatementElement> statements = response.getPolicyDocument().getStatement();
+        assertThat(statements.size(), is(equalTo(SINGLE_STATEMENT_EXPECTED)));
+
+        StatementElement statement = statements.get(UNIQUE_STATEMENT);
+        String actualEffect = statement.getEffect();
+        assertThat(actualEffect, is(equalTo(ACTION_DENIED)));
     }
 
     @Test
@@ -77,9 +93,14 @@ public class LambdaAuthorizerTest implements WithEnvironment {
                 LambdaAuthorizer.AWS_SECRET_KEY_ENV_VAR, WRONG_SECRET_KEY);
 
         Environment envWithWrongSecretName = mockEnvironment(wrongSecretName, DEFAULT_ENV_VALUE);
-        SimpleAuthorizerResponse response = sendRequest(envWithWrongSecretName);
+        AuthorizerResponse response = sendRequest(envWithWrongSecretName);
 
-        assertThat(response.getIsAuthorized(), is(false));
+        List<StatementElement> statements = response.getPolicyDocument().getStatement();
+        assertThat(statements.size(), is(equalTo(SINGLE_STATEMENT_EXPECTED)));
+
+        StatementElement statement = statements.get(UNIQUE_STATEMENT);
+        String actualEffect = statement.getEffect();
+        assertThat(actualEffect, is(equalTo(ACTION_DENIED)));
     }
 
     private AWSSecretsManager secretsManager() {
@@ -89,12 +110,12 @@ public class LambdaAuthorizerTest implements WithEnvironment {
         return awsSecretsManager;
     }
 
-    private SimpleAuthorizerResponse sendRequest(Environment environment) throws IOException {
+    private AuthorizerResponse sendRequest(Environment environment) throws IOException {
         LambdaAuthorizer authorizer = new LambdaAuthorizer(secretsManager(), environment);
         InputStream request = buildRequest();
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         authorizer.handleRequest(request, outputStream, context);
-        return SimpleAuthorizerResponse.fromOutputStream(outputStream);
+        return AuthorizerResponse.fromOutputStream(outputStream);
     }
 
     private InputStream buildRequest() throws JsonProcessingException {

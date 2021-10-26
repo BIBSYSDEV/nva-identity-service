8ed76269d40284c2691a1f6c16a67ff7a0366f90
Jan Bjørndalen <jbj@aztek.no>
2021-10-26 08:32:08 +0200

    Removed debug logging

--- user-access-handlers/src/main/java/no/unit/nva/handlers/authorizer/LambdaAuthorizer.java
+++ user-access-handlers/src/main/java/no/unit/nva/handlers/authorizer/LambdaAuthorizer.java
@@ -1,21 +1,13 @@
 package no.unit.nva.handlers.authorizer;
 
-import static nva.commons.core.attempt.Try.attempt;
-
-import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.secretsmanager.AWSSecretsManager;
 import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import no.unit.commons.apigateway.authentication.AuthorizerResponse;
 import no.unit.commons.apigateway.authentication.RequestAuthorizer;
-import nva.commons.apigateway.RequestInfo;
-import nva.commons.apigateway.exceptions.ApiGatewayException;
 import nva.commons.core.Environment;
 import nva.commons.core.JacocoGenerated;
-import nva.commons.core.JsonUtils;
 import nva.commons.secrets.SecretsReader;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+import static nva.commons.core.attempt.Try.attempt;
 
 public class LambdaAuthorizer extends RequestAuthorizer {
 
@@ -23,9 +15,6 @@ public class LambdaAuthorizer extends RequestAuthorizer {
     public static final String AWS_SECRET_NAME_ENV_VAR = "API_SECRET_NAME";
     public static final String AWS_SECRET_KEY_ENV_VAR = "API_SECRET_KEY";
     private final AWSSecretsManager awsSecretsManager;
-
-    private static final Logger logger = LoggerFactory.getLogger(LambdaAuthorizer.class);
-
 
     @JacocoGenerated
     public LambdaAuthorizer() {
@@ -35,20 +24,6 @@ public class LambdaAuthorizer extends RequestAuthorizer {
     public LambdaAuthorizer(AWSSecretsManager awsSecretsManager, Environment environment) {
         super(environment);
         this.awsSecretsManager = awsSecretsManager;
-    }
-
-    @Override
-    protected AuthorizerResponse processInput(Void input, RequestInfo requestInfo, Context context)
-            throws ApiGatewayException {
-        AuthorizerResponse response = super.processInput(input, requestInfo, context);
-
-        try {
-            logger.debug("Response: " + JsonUtils.dtoObjectMapper.writeValueAsString(response));
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-
-        return response;
     }
 
     @Override

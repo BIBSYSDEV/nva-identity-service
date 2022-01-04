e2ab29e1ef9c409a3fae0612781d763bd557b311
Jan Bjørndalen <jbj@aztek.no>
2022-01-04 13:09:45 +0100

    Removed unused import

--- cognito-pre-token-generation/src/main/java/no/unit/nva/cognito/service/UserService.java
+++ cognito-pre-token-generation/src/main/java/no/unit/nva/cognito/service/UserService.java
@@ -12,7 +12,6 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;

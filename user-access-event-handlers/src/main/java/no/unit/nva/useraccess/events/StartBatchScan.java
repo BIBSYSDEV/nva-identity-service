75bb5c6180376ea10aed0a78354256cc339e3773
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2022-01-19 14:54:56 +0100

    NP-3544: explicitly publish message in event bus

--- user-access-event-handlers/src/main/java/no/unit/nva/useraccess/events/StartBatchScan.java
+++ user-access-event-handlers/src/main/java/no/unit/nva/useraccess/events/StartBatchScan.java
@@ -12,20 +12,37 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.util.Map;
 import no.unit.nva.events.models.ScanDatabaseRequest;
+import nva.commons.core.JacocoGenerated;
+import software.amazon.awssdk.services.eventbridge.EventBridgeClient;
+import software.amazon.awssdk.services.eventbridge.model.PutEventsRequest;
 
 public class StartBatchScan implements RequestStreamHandler {
 
     public static final Map<String, AttributeValue> START_FROM_BEGINNING = null;
+    public static final String DETAIL_TYPE = "topicInDetailType";
+    private  final EventBridgeClient eventClient;
 
+    @JacocoGenerated
     public StartBatchScan() {
+        this(EventBridgeClient.create());
+    }
 
+    public StartBatchScan(EventBridgeClient eventClient) {
+        this.eventClient = eventClient;
     }
 
     @Override
     public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
         ScanDatabaseRequest request = objectMapper.readValue(input, ScanDatabaseRequest.class);
         ScanDatabaseRequest newEvent = createEventAsExpectedByEventListener(request);
+        eventClient.putEvents(createEvent(context, request));
         writeOutput(output, newEvent);
+    }
+
+    private PutEventsRequest createEvent(Context context, ScanDatabaseRequest request) {
+        return PutEventsRequest.builder().entries(request.createNewEventEntry(EventsConfig.EVENT_BUS,
+                                                                              DETAIL_TYPE,
+                                                                              context.getInvokedFunctionArn())).build();
     }
 
     private void writeOutput(OutputStream output, ScanDatabaseRequest newEvent) throws IOException {
--- user-access-event-handlers/src/test/java/no/unit/nva/useraccess/events/StartBatchScanTest.java
+++ user-access-event-handlers/src/test/java/no/unit/nva/useraccess/events/StartBatchScanTest.java
@@ -1,29 +1,35 @@
 package no.unit.nva.useraccess.events;
 
+import static no.unit.nva.testutils.RandomDataGenerator.randomString;
 import static no.unit.nva.useraccess.events.EventsConfig.IDENTITY_SERVICE_BATCH_SCAN_EVENT_TOPIC;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
-import static org.mockito.Mockito.mock;
 import com.amazonaws.services.lambda.runtime.Context;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import no.unit.nva.events.models.ScanDatabaseRequest;
+import no.unit.nva.stubs.FakeContext;
+import no.unit.nva.stubs.FakeEventBridgeClient;
 import nva.commons.core.ioutils.IoUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class StartBatchScanTest {
 
-    public static final Context CONTEXT = mock(Context.class);
+    public static final Context CONTEXT = new FakeContext() {
+        @Override
+        public String getInvokedFunctionArn() {
+            return randomString();
+        }
+    };
     private StartBatchScan batchScanner;
     private ByteArrayOutputStream outputStream;
 
     @BeforeEach
     public void init() {
-        batchScanner = new StartBatchScan();
+        batchScanner = new StartBatchScan(new FakeEventBridgeClient());
         outputStream = new ByteArrayOutputStream();
     }
 

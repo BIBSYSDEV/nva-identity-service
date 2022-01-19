84968212a843ddaeb79571092b09b8829cf43958
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2022-01-19 10:20:52 +0100

    NP-3544: StartBatchScan first iteration

--- user-access-event-handlers/src/main/java/no/unit/nva/useraccess/events/BatchScanner.java
+++
@@ -1,5 +0,0 @@
-package no.unit.nva.useraccess.events;
-
-public class BatchScanner extends RequestHandler{
-
-}
---
+++ user-access-event-handlers/src/test/java/no/unit/nva/useraccess/events/StartBatchScanTest.java
@@ -0,0 +1,37 @@
+package no.unit.nva.useraccess.events;
+
+import static no.unit.nva.useraccess.events.EventsConfig.IDENTITY_SERVICE_BATCH_SCAN_EVENT_TOPIC;
+import static nva.commons.core.attempt.Try.attempt;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.mockito.Mockito.mock;
+import com.amazonaws.services.lambda.runtime.Context;
+import no.unit.nva.events.models.ScanDatabaseRequest;
+import no.unit.nva.stubs.FakeEventBridgeClient;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class StartBatchScanTest {
+
+    public static final Context CONTEXT = mock(Context.class);
+    private StartBatchScan batchScanner;
+
+    @BeforeEach
+    public void init() {
+        batchScanner = new StartBatchScan();
+    }
+
+    @Test
+    void shouldSendEventContainingTheEventTypeAndNullStartingPointWhenInputIsAnEmptyObject() {
+        var actualEntry=batchScanner.handleRequest(emptyInput(), CONTEXT);
+        var expectedEntry = new ScanDatabaseRequest(IDENTITY_SERVICE_BATCH_SCAN_EVENT_TOPIC,
+                                                    ScanDatabaseRequest.DEFAULT_PAGE_SIZE,
+                                                    null);
+        assertThat(actualEntry,is(equalTo(expectedEntry)));
+    }
+
+    private ScanDatabaseRequest emptyInput() {
+        return new ScanDatabaseRequest(null, null, null);
+    }
+}

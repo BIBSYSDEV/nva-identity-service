ddc81757c04aba6082929f0e293de72e7705198e
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2022-01-19 14:05:29 +0100

    NP-3544: fix parsing in StartBatchScan handler

--- user-access-event-handlers/src/test/java/no/unit/nva/useraccess/events/StartBatchScanTest.java
+++ user-access-event-handlers/src/test/java/no/unit/nva/useraccess/events/StartBatchScanTest.java
@@ -6,7 +6,12 @@ import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.mockito.Mockito.mock;
 import com.amazonaws.services.lambda.runtime.Context;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import no.unit.nva.events.models.ScanDatabaseRequest;
+import nva.commons.core.ioutils.IoUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -14,22 +19,27 @@ class StartBatchScanTest {
 
     public static final Context CONTEXT = mock(Context.class);
     private StartBatchScan batchScanner;
+    private ByteArrayOutputStream outputStream;
 
     @BeforeEach
     public void init() {
         batchScanner = new StartBatchScan();
+        outputStream = new ByteArrayOutputStream();
     }
 
     @Test
-    void shouldSendEventContainingTheEventTypeAndNullStartingPointWhenInputIsAnEmptyObject() {
-        var actualEntry = batchScanner.handleRequest(emptyInput(), CONTEXT);
+    void shouldSendEventContainingTheEventTypeAndNullStartingPointWhenInputIsAnEmptyObject()
+        throws IOException {
+        batchScanner.handleRequest(emptyInput(), outputStream, CONTEXT);
+        var actualEntry = ScanDatabaseRequest.fromJson(outputStream.toString());
         var expectedEntry = new ScanDatabaseRequest(IDENTITY_SERVICE_BATCH_SCAN_EVENT_TOPIC,
                                                     ScanDatabaseRequest.DEFAULT_PAGE_SIZE,
                                                     null);
         assertThat(actualEntry, is(equalTo(expectedEntry)));
     }
 
-    private ScanDatabaseRequest emptyInput() {
-        return new ScanDatabaseRequest(null, null, null);
+    private InputStream emptyInput() {
+        var request = new ScanDatabaseRequest(null, null, null);
+        return IoUtils.stringToStream(request.toJsonString());
     }
 }

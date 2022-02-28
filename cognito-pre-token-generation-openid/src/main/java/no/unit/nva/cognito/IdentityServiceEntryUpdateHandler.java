6aa4d1fe5cd3655a13025f96bea09bd2a13ade82
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2022-02-28 12:53:28 +0100

    pretoken handler is echo hanlder

--- cognito-pre-token-generation-openid/src/main/java/no/unit/nva/cognito/IdentityServiceEntryUpdateHandler.java
+++ cognito-pre-token-generation-openid/src/main/java/no/unit/nva/cognito/IdentityServiceEntryUpdateHandler.java
@@ -4,13 +4,12 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.CognitoUserPoolPreTokenGenerationEvent;
 
-public class UserPoolEntryUpdateHandler
+public class IdentityServiceEntryUpdateHandler
     implements RequestHandler<CognitoUserPoolPreTokenGenerationEvent, CognitoUserPoolPreTokenGenerationEvent> {
 
     @Override
     public CognitoUserPoolPreTokenGenerationEvent handleRequest(CognitoUserPoolPreTokenGenerationEvent input,
                                                                 Context context) {
-        context.getLogger().log(input.toString());
-        return input;
+        return null;
     }
 }

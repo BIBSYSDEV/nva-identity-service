258b6d6d2287eee49661a3493007f4455735d2f5
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2022-03-06 21:16:49 +0100

    getUser returns User

--- user-access-handlers/src/main/java/no/unit/nva/handlers/GetUserHandler.java
+++ user-access-handlers/src/main/java/no/unit/nva/handlers/GetUserHandler.java
@@ -3,10 +3,14 @@ package no.unit.nva.handlers;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import java.net.HttpURLConnection;
+import java.net.URI;
+import no.unit.nva.database.IdentityService;
+import no.unit.nva.database.IdentityServiceImpl;
+import no.unit.nva.useraccessmanagement.model.UserDto;
 import nva.commons.apigatewayv2.ApiGatewayHandlerV2;
 import nva.commons.core.JacocoGenerated;
 
-public class GetUserHandler extends ApiGatewayHandlerV2<Void, String> {
+public class GetUserHandler extends ApiGatewayHandlerV2<Void, UserDto> {
 
 //    private final IdentityService databaseService;
 
@@ -16,30 +20,31 @@ public class GetUserHandler extends ApiGatewayHandlerV2<Void, String> {
 //        this(new IdentityServiceImpl());
     }
 
-//    public GetUserHandler(IdentityService databaseService) {
-//        super();
-////        this.databaseService = databaseService;
-//
-//    }
+    public GetUserHandler(IdentityService databaseService) {
+        super();
+//        this.databaseService = databaseService;
+
+    }
 
     @Override
-    protected String processInput(String input, APIGatewayProxyRequestEvent requestInfo, Context context) {
+    protected UserDto processInput(String input, APIGatewayProxyRequestEvent requestInfo, Context context) {
 
 //        String username = extractValidUserNameOrThrowException(requestInfo);
 //        UserDto queryObject = UserDto.newBuilder().withUsername(username).build();
-//        UserDto userDto = UserDto.newBuilder()
-//                              .withUsername("randomname")
-//                              .withFamilyName("Gkorgkas")
-//                              .withUsername("og@unit.no")
-//                              .withInstitution(URI.create("https://example.com"))
-//                              .build();
+        UserDto userDto = UserDto.newBuilder()
+                              .withUsername("randomname")
+                              .withFamilyName("Gkorgkas")
+                              .withUsername("og@unit.no")
+                              .withInstitution(URI.create("https://example.com"))
+                              .build();
 
-        return "hello world";
+        return userDto;
+
 //        return databaseService.getUser(queryObject);
     }
 
     @Override
-    protected Integer getSuccessStatusCode(String input, String output) {
+    protected Integer getSuccessStatusCode(String input, UserDto output) {
         return HttpURLConnection.HTTP_OK;
     }
 

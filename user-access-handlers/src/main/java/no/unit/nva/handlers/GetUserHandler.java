25a0c8f7c5ae37ae56496dc0146bd5f3192ebad5
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2022-03-06 23:19:54 +0100

    restore functionality

--- user-access-handlers/src/main/java/no/unit/nva/handlers/GetUserHandler.java
+++ user-access-handlers/src/main/java/no/unit/nva/handlers/GetUserHandler.java
@@ -1,46 +1,29 @@
 package no.unit.nva.handlers;
 
+import static java.util.function.Predicate.not;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import java.net.HttpURLConnection;
-import java.net.URI;
+import java.util.Optional;
 import no.unit.nva.database.IdentityService;
+import no.unit.nva.database.IdentityServiceImpl;
 import no.unit.nva.useraccessmanagement.model.UserDto;
-import nva.commons.apigatewayv2.ApiGatewayHandlerV2;
+import nva.commons.apigatewayv2.exceptions.BadRequestException;
 import nva.commons.core.JacocoGenerated;
 
 @SuppressWarnings("PMD.UnusedFormalParameter")
-public class GetUserHandler extends ApiGatewayHandlerV2<Void, UserDto> {
+public class GetUserHandler extends HandlerAccessingUser<Void, UserDto> {
 
-//    private final IdentityService databaseService;
+    private final IdentityService databaseService;
 
     @JacocoGenerated
     public GetUserHandler() {
-        super();
-//        this(new IdentityServiceImpl());
+        this(new IdentityServiceImpl());
     }
 
     public GetUserHandler(IdentityService databaseService) {
         super();
-//        this.databaseService = databaseService;
-
-    }
-
-    @Override
-    protected UserDto processInput(String input, APIGatewayProxyRequestEvent requestInfo, Context context) {
-
-//        String username = extractValidUserNameOrThrowException(requestInfo);
-//        UserDto queryObject = UserDto.newBuilder().withUsername(username).build();
-        UserDto userDto = UserDto.newBuilder()
-                              .withUsername("randomname")
-                              .withFamilyName("Gkorgkas")
-                              .withUsername("og@unit.no")
-                              .withInstitution(URI.create("https://example.com"))
-                              .build();
-
-        return userDto;
-
-//        return databaseService.getUser(queryObject);
+        this.databaseService = databaseService;
     }
 
     @Override
@@ -48,12 +31,21 @@ public class GetUserHandler extends ApiGatewayHandlerV2<Void, UserDto> {
         return HttpURLConnection.HTTP_OK;
     }
 
-//    private String extractValidUserNameOrThrowException(APIGatewayProxyRequestEvent requestInfo) {
-//        return Optional.of(requestInfo)
-//            .map(APIGatewayProxyRequestEvent::getPathParameters)
-//            .map(map -> map.get(USERNAME_PATH_PARAMETER))
-//            .map(this::decodeUrlPart)
-//            .filter(not(String::isBlank))
-//            .orElseThrow(() -> new BadRequestException(EMPTY_USERNAME_PATH_PARAMETER_ERROR));
-//    }
+    @Override
+    protected UserDto processInput(String input, APIGatewayProxyRequestEvent requestInfo, Context context) {
+
+        String username = extractValidUserNameOrThrowException(requestInfo);
+        UserDto queryObject = UserDto.newBuilder().withUsername(username).build();
+
+        return databaseService.getUser(queryObject);
+    }
+
+    private String extractValidUserNameOrThrowException(APIGatewayProxyRequestEvent requestInfo) {
+        return Optional.of(requestInfo)
+            .map(APIGatewayProxyRequestEvent::getPathParameters)
+            .map(map -> map.get(USERNAME_PATH_PARAMETER))
+            .map(this::decodeUrlPart)
+            .filter(not(String::isBlank))
+            .orElseThrow(() -> new BadRequestException(EMPTY_USERNAME_PATH_PARAMETER_ERROR));
+    }
 }

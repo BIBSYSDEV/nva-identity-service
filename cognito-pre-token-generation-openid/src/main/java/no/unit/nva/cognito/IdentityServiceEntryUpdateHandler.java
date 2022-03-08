81aeb7bae07466750b74153994371e77cf52cd9d
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2022-03-08 08:19:14 +0100

    fix AllowedOAuthFlows

--- cognito-pre-token-generation-openid/src/main/java/no/unit/nva/cognito/IdentityServiceEntryUpdateHandler.java
+++ cognito-pre-token-generation-openid/src/main/java/no/unit/nva/cognito/IdentityServiceEntryUpdateHandler.java
@@ -5,13 +5,31 @@ import static nva.commons.core.attempt.Try.attempt;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.CognitoUserPoolPreTokenGenerationEvent;
+import nva.commons.core.Environment;
+import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient;
+import software.amazon.awssdk.services.cognitoidentityprovider.model.DescribeUserPoolClientRequest;
 
 public class IdentityServiceEntryUpdateHandler
     implements RequestHandler<CognitoUserPoolPreTokenGenerationEvent, CognitoUserPoolPreTokenGenerationEvent> {
 
+    private static final String BACKEND_CLIENT_ID = new Environment().readEnv("BACKEND_CLIENT_ID");
+    private final CognitoIdentityProviderClient cognitoClient;
+
+    public IdentityServiceEntryUpdateHandler(CognitoIdentityProviderClient cognitoClient){
+
+        this.cognitoClient = cognitoClient;
+    }
+
     @Override
     public CognitoUserPoolPreTokenGenerationEvent handleRequest(CognitoUserPoolPreTokenGenerationEvent input,
                                                                 Context context) {
+        var clientSecret=cognitoClient
+            .describeUserPoolClient(DescribeUserPoolClientRequest.builder().clientId(BACKEND_CLIENT_ID).build())
+            .userPoolClient()
+            .clientSecret();
+
+        context.getLogger().log(clientSecret);
+
 
         String userAttributes =
             attempt(() -> objectMapper.asString(input.getRequest().getUserAttributes().entrySet()))
---
+++ cognito-pre-token-generation-openid/src/test/java/no/unit/nva/cognito/IdentityServiceEntryUpdateHandlerTest.java
@@ -0,0 +1,13 @@
+package no.unit.nva.cognito;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IdentityServiceEntryUpdateHandlerTest {
+
+
+
+    void shouldRetrieveCognitoBackendClientSecret(){
+
+    }
+
+}

2499ca45528eb0710d45539282dcc101263f4439
torarnet <6398417+torarnet@users.noreply.github.com>
2022-05-03 13:53:19 +0200

    Json friendly enum

--- customer-commons/src/main/java/no/unit/nva/customer/model/PublicationWorkflow.java
+++ customer-commons/src/main/java/no/unit/nva/customer/model/PublicationWorkflow.java
@@ -1,8 +1,22 @@
 package no.unit.nva.customer.model;
 
+import static java.lang.String.format;
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.joining;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
+import nva.commons.core.SingletonCollector;
+import nva.commons.core.attempt.Failure;
+
 public enum PublicationWorkflow {
+    @JsonProperty("RegistratorPublishesMetadataOnly")
     REGISTRATOR_PUBLISHES_METADATA_ONLY("RegistratorPublishesMetadataOnly"),
+    @JsonProperty("RegistratorPublishesMetadataAndFiles")
     REGISTRATOR_PUBLISHES_METADATA_AND_FILES("RegistratorPublishesMetadataAndFiles");
+
+    public static final String ERROR_MESSAGE_TEMPLATE = "%s not a valid PublicationWorkflow, expected one of: %s";
+    public static final String DELIMITER = ", ";
 
     private final String value;
 
@@ -10,7 +24,22 @@ public enum PublicationWorkflow {
         this.value = value;
     }
 
+    @JsonValue
     public String getValue() {
         return value;
+    }
+
+    @JsonCreator
+    public static PublicationWorkflow lookUp(String value) {
+        return  stream(values())
+                    .filter(nameType -> nameType.getValue().equalsIgnoreCase(value))
+                    .collect(SingletonCollector.tryCollect())
+                    .orElseThrow(failure -> throwException(failure, value));
+    }
+
+    private static RuntimeException throwException(Failure<PublicationWorkflow> failure, String value) {
+        return new IllegalArgumentException(
+            format(ERROR_MESSAGE_TEMPLATE, value, stream(PublicationWorkflow.values())
+                                                      .map(PublicationWorkflow::toString).collect(joining(DELIMITER))));
     }
 }

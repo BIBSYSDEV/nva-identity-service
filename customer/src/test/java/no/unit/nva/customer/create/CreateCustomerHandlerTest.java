881fa50338822c3f15cddb34f5a2e3d497509535
torarnet <6398417+torarnet@users.noreply.github.com>
2022-05-04 15:09:54 +0200

    NP-9101: Add bad request test

--- customer/src/test/java/no/unit/nva/customer/create/CreateCustomerHandlerTest.java
+++ customer/src/test/java/no/unit/nva/customer/create/CreateCustomerHandlerTest.java
@@ -1,22 +1,6 @@
 package no.unit.nva.customer.create;
 
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
-import no.unit.nva.customer.model.CustomerDto;
-import no.unit.nva.customer.service.CustomerService;
-import no.unit.nva.customer.service.impl.DynamoDBCustomerService;
-import no.unit.nva.customer.testing.LocalCustomerServiceDatabase;
-import no.unit.nva.identityservice.json.JsonConfig;
-import no.unit.nva.stubs.FakeContext;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import java.net.HttpURLConnection;
-import java.util.Collections;
-import java.util.Map;
-
+import static java.net.HttpURLConnection.HTTP_BAD_REQUEST;
 import static no.unit.nva.customer.model.PublicationWorkflow.REGISTRATOR_PUBLISHES_METADATA_AND_FILES;
 import static no.unit.nva.customer.model.PublicationWorkflow.REGISTRATOR_PUBLISHES_METADATA_ONLY;
 import static no.unit.nva.customer.testing.TestHeaders.getRequestHeaders;
@@ -26,6 +10,21 @@ import static nva.commons.core.attempt.Try.attempt;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import java.net.HttpURLConnection;
+import java.util.Collections;
+import java.util.Map;
+import no.unit.nva.customer.model.CustomerDto;
+import no.unit.nva.customer.service.CustomerService;
+import no.unit.nva.customer.service.impl.DynamoDBCustomerService;
+import no.unit.nva.customer.testing.LocalCustomerServiceDatabase;
+import no.unit.nva.identityservice.json.JsonConfig;
+import no.unit.nva.stubs.FakeContext;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class CreateCustomerHandlerTest extends LocalCustomerServiceDatabase {
 
@@ -69,10 +68,10 @@ public class CreateCustomerHandlerTest extends LocalCustomerServiceDatabase {
     @Test
     void shouldReturnPublicationWorkflowWhenValueIsSet() {
         var customerDto = CustomerDto.builder()
-                .withName("New Customer")
-                .withVocabularies(Collections.emptySet())
-                .withPublicationWorkflow(REGISTRATOR_PUBLISHES_METADATA_ONLY)
-                .build();
+                              .withName("New Customer")
+                              .withVocabularies(Collections.emptySet())
+                              .withPublicationWorkflow(REGISTRATOR_PUBLISHES_METADATA_ONLY)
+                              .build();
         var requestBody = CreateCustomerRequest.fromCustomerDto(customerDto);
         var response = executeRequest(requestBody);
         var actualResponseBody = CustomerDto.fromJson(response.getBody());
@@ -80,12 +79,25 @@ public class CreateCustomerHandlerTest extends LocalCustomerServiceDatabase {
     }
 
     @Test
+    void shouldReturnPublicationWorkflowErrorWhenValueIsWrong() {
+        var customerDto = CustomerDto.builder()
+                              .withName("New Customer")
+                              .withVocabularies(Collections.emptySet())
+                              .withPublicationWorkflow(REGISTRATOR_PUBLISHES_METADATA_ONLY)
+                              .build();
+        var requestBody = CreateCustomerRequest.fromCustomerDto(customerDto).toString()
+                              .replace(REGISTRATOR_PUBLISHES_METADATA_ONLY.getValue(), "hello");
+        var response = executeRequest(requestBody);
+        assertThat(response.getStatusCode(), is(equalTo(HTTP_BAD_REQUEST)));
+    }
+
+    @Test
     void shouldReturnBadRequestWhenInputIsNotAValidJson() {
         var input = new APIGatewayProxyRequestEvent()
-            .withBody(randomString())
-            .withHeaders(getRequestHeaders());
+                        .withBody(randomString())
+                        .withHeaders(getRequestHeaders());
         var response = handler.handleRequest(input, context);
-        assertThat(response.getStatusCode(), is(equalTo(HttpURLConnection.HTTP_BAD_REQUEST)));
+        assertThat(response.getStatusCode(), is(equalTo(HTTP_BAD_REQUEST)));
     }
 
     @Test
@@ -95,20 +107,27 @@ public class CreateCustomerHandlerTest extends LocalCustomerServiceDatabase {
             .withBody(attempt(() -> JsonConfig.writeValueAsString(body)).orElseThrow())
             .withHeaders(getRequestHeaders());
         var response = handler.handleRequest(input, context);
-        assertThat(response.getStatusCode(), is(equalTo(HttpURLConnection.HTTP_BAD_REQUEST)));
+        assertThat(response.getStatusCode(), is(equalTo(HTTP_BAD_REQUEST)));
     }
 
     private APIGatewayProxyResponseEvent executeRequest(CreateCustomerRequest request) {
         var input = new APIGatewayProxyRequestEvent()
-                .withBody(request.toString())
-                .withHeaders(getRequestHeaders());
+                        .withBody(request.toString())
+                        .withHeaders(getRequestHeaders());
+        return handler.handleRequest(input, context);
+    }
+
+    private APIGatewayProxyResponseEvent executeRequest(String request) {
+        var input = new APIGatewayProxyRequestEvent()
+                        .withBody(request)
+                        .withHeaders(getRequestHeaders());
         return handler.handleRequest(input, context);
     }
 
     private CustomerDto validCustomerDto() {
         return CustomerDto.builder()
-                .withName("New Customer")
-                .withVocabularies(Collections.emptySet())
-                .build();
+                   .withName("New Customer")
+                   .withVocabularies(Collections.emptySet())
+                   .build();
     }
 }

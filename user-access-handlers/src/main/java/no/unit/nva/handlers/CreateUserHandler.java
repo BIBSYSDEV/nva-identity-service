8839040ac1f2e6e88179f45593034dc2faa92a1a
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2022-05-05 10:28:12 +0200

    wip

--- user-access-handlers/src/main/java/no/unit/nva/handlers/CreateUserHandler.java
+++ user-access-handlers/src/main/java/no/unit/nva/handlers/CreateUserHandler.java
@@ -3,8 +3,10 @@ package no.unit.nva.handlers;
 import com.amazonaws.services.lambda.runtime.Context;
 import java.net.HttpURLConnection;
 import no.unit.nva.handlers.models.CreateUserRequest;
+import no.unit.nva.useraccessservice.accessrights.AccessRight;
 import no.unit.nva.useraccessservice.model.UserDto;
 import nva.commons.apigateway.RequestInfo;
+import nva.commons.apigateway.exceptions.ForbiddenException;
 import nva.commons.core.JacocoGenerated;
 
 public class CreateUserHandler extends HandlerWithEventualConsistency<CreateUserRequest, UserDto> {
@@ -20,9 +22,19 @@ public class CreateUserHandler extends HandlerWithEventualConsistency<CreateUser
     }
 
     @Override
-    protected UserDto processInput(CreateUserRequest input, RequestInfo requestInfo, Context context) {
+    protected UserDto processInput(CreateUserRequest input, RequestInfo requestInfo, Context context)
+        throws ForbiddenException {
+        authorize(requestInfo);
         return UserDto.newBuilder()
             .withInstitution(input.getCustomerId())
             .build();
+    }
+
+    private void authorize(RequestInfo requestInfo) throws ForbiddenException {
+        if(!requestInfo.userIsAuthorized(AccessRight.EDIT_OWN_INSTITUTION_USERS.toString())){
+            throw new ForbiddenException();
+        }
+
+
     }
 }

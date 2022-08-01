c12c0c4a5d5f6599d0fadbf56ba82969e8989dd4
anetteOlli <anette.olli.siiri@sikt.no>
2022-08-01 13:30:03 +0200

    NP-9328-course-access-checker, added wiremock

--- fsproxy/src/main/java/no/unit/identityservice/fsproxy/FsApi.java
+++ fsproxy/src/main/java/no/unit/identityservice/fsproxy/FsApi.java
@@ -1,14 +1,5 @@
 package no.unit.identityservice.fsproxy;
 
-import no.unit.identityservice.fsproxy.model.FsIdNumber;
-import no.unit.identityservice.fsproxy.model.FsNin;
-import no.unit.identityservice.fsproxy.model.FsPersonSearchResponse;
-import no.unit.identityservice.fsproxy.model.FsCourseData;
-import no.unit.identityservice.fsproxy.model.FsCoursesSearchResult;
-import no.unit.nva.commons.json.JsonUtils;
-import nva.commons.core.Environment;
-import nva.commons.core.paths.UriWrapper;
-import org.joda.time.DateTime;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -16,6 +7,16 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Base64;
 import java.util.List;
+import no.unit.identityservice.fsproxy.model.FsCourseData;
+import no.unit.identityservice.fsproxy.model.FsCoursesSearchResult;
+import no.unit.identityservice.fsproxy.model.FsIdNumber;
+import no.unit.identityservice.fsproxy.model.FsNin;
+import no.unit.identityservice.fsproxy.model.FsPersonSearchResponse;
+import no.unit.nva.commons.json.JsonUtils;
+import nva.commons.core.Environment;
+import nva.commons.core.JacocoGenerated;
+import nva.commons.core.paths.UriWrapper;
+import org.joda.time.DateTime;
 
 
 public class FsApi {
@@ -31,6 +32,18 @@ public class FsApi {
     private static final String LIMIT_IDENTIFIER = "limit";
     private final String username = new Environment().readEnv("FS_USERNAME");
     private final String password = new Environment().readEnv("FS_PASSWORD");
+    private final HttpClient httpClient;
+    private final String baseFsHostUrl;
+
+    public FsApi (HttpClient httpClient, String baseFsHostUrl) {
+        this.httpClient = httpClient;
+        this.baseFsHostUrl = baseFsHostUrl;
+    }
+
+    @JacocoGenerated
+    public FsApi() {
+        this(HttpClient.newBuilder().build(), new Environment().readEnv("FS_HOST") );
+    }
 
     public FsIdNumber getFsId(FsNin nationalIdentityNumber) throws IOException, InterruptedException {
         var httpClient = HttpClient.newBuilder().build();
@@ -57,7 +70,7 @@ public class FsApi {
     }
 
     private URI createSearchPersonUri(FsNin nin) {
-        return UriWrapper.fromHost(FS_HOST)
+        return UriWrapper.fromHost(baseFsHostUrl)
                 .addChild(PERSON_PATH)
                 .addQueryParameter(DB_IDENTIFIER, "true")
                 .addQueryParameter(LIMIT_IDENTIFIER, "0")
@@ -71,7 +84,7 @@ public class FsApi {
         final String year = String.valueOf(new DateTime().getYear());
         final String lopenummer = getFsId(nin).toString();
 
-        return UriWrapper.fromHost(FS_HOST)
+        return UriWrapper.fromHost(baseFsHostUrl)
                 .addChild(STUDENTUNDERVISNING_PATH)
                 .addQueryParameter(DB_IDENTIFIER, "true")
                 .addQueryParameter(LIMIT_IDENTIFIER, "0")
--- fsproxy/src/test/java/no/unit/identityservice/fsproxy/FsApiTest.java
+++ fsproxy/src/test/java/no/unit/identityservice/fsproxy/FsApiTest.java
@@ -1,60 +1,92 @@
 package no.unit.identityservice.fsproxy;
 
-import no.unit.identityservice.fsproxy.model.*;
-import org.junit.jupiter.api.Test;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.ok;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.core.Is.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.MappingBuilder;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import no.unit.identityservice.fsproxy.model.FsCourse;
+import no.unit.identityservice.fsproxy.model.FsCourseData;
+import no.unit.identityservice.fsproxy.model.FsEmne;
+import no.unit.identityservice.fsproxy.model.FsIdNumber;
+import no.unit.identityservice.fsproxy.model.FsNin;
+import no.unit.identityservice.fsproxy.model.FsSemester;
+import no.unit.identityservice.fsproxy.model.FsUndervisning;
+import no.unit.nva.stubs.WiremockHttpClient;
+import nva.commons.core.ioutils.IoUtils;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class FsApiTest {
 
     private static final FsCourseData TEST_COURSE_1 = new FsCourseData(
-            new FsCourse(
-                    new FsUndervisning(
-                            new FsEmne("ABIO6050"), 1, new FsSemester("2022", "VÅR"))));
+        new FsCourse(
+            new FsUndervisning(
+                new FsEmne("ABIO6050"), 1, new FsSemester("2022", "VÅR"))));
     private static final FsCourseData TEST_COURSE_2 = new FsCourseData(
-            new FsCourse(
-                    new FsUndervisning(
-                            new FsEmne("ABIO6100"), 1, new FsSemester("2022", "VÅR"))));
+        new FsCourse(
+            new FsUndervisning(
+                new FsEmne("ABIO6100"), 1, new FsSemester("2022", "VÅR"))));
     private static final FsCourseData TEST_COURSE_3 = new FsCourseData(
-            new FsCourse(
-                    new FsUndervisning(
-                            new FsEmne("OPERA6000"), 1, new FsSemester("2022", "VÅR"))));
+        new FsCourse(
+            new FsUndervisning(
+                new FsEmne("OPERA6000"), 1, new FsSemester("2022", "VÅR"))));
     private static final FsCourseData TEST_COURSE_4 = new FsCourseData(
-            new FsCourse(
-                    new FsUndervisning(
-                            new FsEmne("OPERA6000"), 2, new FsSemester("2022", "HØST"))));
+        new FsCourse(
+            new FsUndervisning(
+                new FsEmne("OPERA6000"), 2, new FsSemester("2022", "HØST"))));
     private static final FsCourseData TEST_COURSE_5 = new FsCourseData(
-            new FsCourse(
-                    new FsUndervisning(
-                            new FsEmne("ABIO6100"), 2, new FsSemester("2022", "VÅR"))));
+        new FsCourse(
+            new FsUndervisning(
+                new FsEmne("ABIO6100"), 2, new FsSemester("2022", "VÅR"))));
     private static final FsCourseData TEST_COURSE_6 = new FsCourseData(
-            new FsCourse(
-                    new FsUndervisning(
-                            new FsEmne("ABIO6100"), 3, new FsSemester("2022", "HØST"))));
+        new FsCourse(
+            new FsUndervisning(
+                new FsEmne("ABIO6100"), 3, new FsSemester("2022", "HØST"))));
     private static final FsCourseData TEST_COURSE_7 = new FsCourseData(
-            new FsCourse(
-                    new FsUndervisning(
-                            new FsEmne("OPERAPRA20"), 1, new FsSemester("2022", "VÅR"))));
+        new FsCourse(
+            new FsUndervisning(
+                new FsEmne("OPERAPRA20"), 1, new FsSemester("2022", "VÅR"))));
     private static final FsCourseData TEST_COURSE_8 = new FsCourseData(
-            new FsCourse(
-                    new FsUndervisning(
-                            new FsEmne("OPERAPRA3"), 1, new FsSemester("2022", "HØST"))));
+        new FsCourse(
+            new FsUndervisning(
+                new FsEmne("OPERAPRA3"), 1, new FsSemester("2022", "HØST"))));
+    private HttpClient httpClient;
+    private WireMockServer httpServer;
+    private URI fsUrl;
+    private FsApi fsApi;
+
+    @AfterEach
+    public void tearDown() {
+        httpServer.stop();
+    }
+
+    @BeforeEach
+    void init() {
+        startWiremockServer();
+    }
 
     @Test
     void shouldReturnFsIdNumberWhenInputIsNin() throws IOException, InterruptedException {
         var expectedFsIdNumber = new FsIdNumber(78);
         var nin = new FsNin("24027336201");
-        FsApi fsApi = new FsApi();
+        String response = IoUtils.stringFromResources(Path.of("CoursesToStudent.json"));
+        createTestStubForGetFsPersonId("190477", "47298", response);
         var actualIdNumber = fsApi.getFsId(nin);
-
         assertThat(actualIdNumber, is(equalTo(expectedFsIdNumber)));
     }
 
@@ -71,20 +103,21 @@ public class FsApiTest {
         expectedCourses.add(TEST_COURSE_7);
         expectedCourses.add(TEST_COURSE_8);
         var expectedCoursesEmner = expectedCourses.stream()
-                .map(item -> item.getFsCourse().getUndervisning().getEmne().getCode())
-                .collect(Collectors.toList());
-        FsApi fsApi = new FsApi();
+                                       .map(item -> item.getFsCourse().getUndervisning().getEmne().getCode())
+                                       .collect(Collectors.toList());
         var actualCoursesEmner = fsApi.getCourses(nin)
-                .stream()
-                .map(item -> item.getFsCourse().getUndervisning().getEmne().getCode())
-                .collect(Collectors.toList());
+                                     .stream()
+                                     .map(item -> item.getFsCourse().getUndervisning().getEmne().getCode())
+                                     .collect(Collectors.toList());
+
+
 
         assertTrue(actualCoursesEmner.containsAll(expectedCoursesEmner));
     }
 
     @Test
     void shouldReturnSameAmountOfCoursesOfCurrentYearWhenInputIsNinOfStudent()
-            throws IOException, InterruptedException {
+        throws IOException, InterruptedException {
         var nin = new FsNin("19047747298");
         List<FsCourseData> expectedCourses = new ArrayList<>();
         expectedCourses.add(TEST_COURSE_1);
@@ -95,7 +128,7 @@ public class FsApiTest {
         expectedCourses.add(TEST_COURSE_6);
         expectedCourses.add(TEST_COURSE_7);
         expectedCourses.add(TEST_COURSE_8);
-        var actualCourses = new FsApi().getCourses(nin);
+        var actualCourses = fsApi.getCourses(nin);
 
         assertEquals(expectedCourses.size(), actualCourses.size());
     }
@@ -104,27 +137,43 @@ public class FsApiTest {
     void shouldReturnUndervisningDataWhenJsonIsDeserialized() throws IOException, InterruptedException {
         var nin = new FsNin("19047747298");
         String expectedFsYear = new FsUndervisning(new FsEmne("ABIO6050"), 1,
-                new FsSemester("2022", "VÅR")).getSemester().getYear();
+                                                   new FsSemester("2022", "VÅR")).getSemester().getYear();
         String expectedFsTermin = new FsUndervisning(new FsEmne("ABIO6050"), 1,
-                new FsSemester("2022", "VÅR")).getSemester().getTermin();
+                                                     new FsSemester("2022", "VÅR")).getSemester().getTermin();
         int expectedFsTerminNumber = new FsUndervisning(new FsEmne("ABIO6050"), 1,
-                new FsSemester("2022", "VÅR")).getTerminNumber();
+                                                        new FsSemester("2022", "VÅR")).getTerminNumber();
 
-        FsApi fsApi = new FsApi();
         var courses = fsApi.getCourses(nin);
         var actualFsUndervisningYears = courses.stream()
-                .map(item -> item.getFsCourse().getUndervisning().getSemester().getYear())
-                .collect(Collectors.toList());
+                                            .map(item -> item.getFsCourse().getUndervisning().getSemester().getYear())
+                                            .collect(Collectors.toList());
         var actualFsUndervisningTermins = courses.stream()
-                .map(item -> item.getFsCourse().getUndervisning().getSemester().getTermin())
-                .collect(Collectors.toList());
+                                              .map(item -> item.getFsCourse()
+                                                               .getUndervisning()
+                                                               .getSemester()
+                                                               .getTermin())
+                                              .collect(Collectors.toList());
         var actualFsUndervisningTerminNumbers = courses.stream()
-                .map(item -> item.getFsCourse().getUndervisning().getTerminNumber())
-                .collect(Collectors.toList());
+                                                    .map(item -> item.getFsCourse().getUndervisning().getTerminNumber())
+                                                    .collect(Collectors.toList());
 
         assertTrue(actualFsUndervisningYears.contains(expectedFsYear));
         assertTrue(actualFsUndervisningTermins.contains(expectedFsTermin));
         assertTrue(actualFsUndervisningTerminNumbers.contains(expectedFsTerminNumber));
+    }
 
+    private void startWiremockServer() {
+        httpServer = new WireMockServer(options().dynamicHttpsPort());
+        httpServer.start();
+        fsUrl = URI.create(httpServer.baseUrl());
+        httpClient = WiremockHttpClient.create();
+        fsApi = new FsApi(httpClient, httpServer.baseUrl());
+    }
+
+    private void createTestStubForGetFsPersonId(String birthdate, String personalNumber, String responseBody) {
+        httpServer.stubFor(
+            get(urlEqualTo(httpServer.baseUrl() + "/personer?dbId=true&limit=0&fodselsdato0=" + birthdate +
+                           "&personnummer0=" + personalNumber)).willReturn(
+                ok().withHeader("Content-Type", "application/json").withBody(responseBody)));
     }
 }

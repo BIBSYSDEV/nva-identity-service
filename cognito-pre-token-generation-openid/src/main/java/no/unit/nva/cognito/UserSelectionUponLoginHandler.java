7722932782d411e6dbff6939a1d823a64a49ba6c
Ketil Aasarød <ketil.aasarod@sikt.no>
2022-11-03 15:20:24 +0100

    temporarly log custom claims we are adding to cognito

--- cognito-pre-token-generation-openid/src/main/java/no/unit/nva/cognito/UserSelectionUponLoginHandler.java
+++ cognito-pre-token-generation-openid/src/main/java/no/unit/nva/cognito/UserSelectionUponLoginHandler.java
@@ -43,6 +43,8 @@ import no.unit.nva.useraccessservice.usercreation.cristin.person.CristinClient;
 import nva.commons.core.Environment;
 import nva.commons.core.JacocoGenerated;
 import nva.commons.core.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
 import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient;
 import software.amazon.awssdk.regions.Region;
@@ -52,6 +54,8 @@ import software.amazon.awssdk.services.cognitoidentityprovider.model.AttributeTy
 
 public class UserSelectionUponLoginHandler
     implements RequestHandler<CognitoUserPoolPreTokenGenerationEvent, CognitoUserPoolPreTokenGenerationEvent> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserSelectionUponLoginHandler.class);
 
     public static final Environment ENVIRONMENT = new Environment();
 
@@ -166,10 +170,14 @@ public class UserSelectionUponLoginHandler
         Collection<String> accessRights,
         Collection<String> roles) {
 
+        Collection<AttributeType> userAttributes = updatedPersonAttributes(authenticationInfo, accessRights, roles);
+
+        LOGGER.info("Updating user attributes: {}", userAttributes);
+
         return AdminUpdateUserAttributesRequest.builder()
                    .userPoolId(input.getUserPoolId())
                    .username(input.getUserName())
-                   .userAttributes(updatedPersonAttributes(authenticationInfo, accessRights, roles))
+                   .userAttributes(userAttributes)
                    .build();
     }
 

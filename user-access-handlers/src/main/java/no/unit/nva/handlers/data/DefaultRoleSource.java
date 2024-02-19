eb2786fa618d27b3f1d42ac812ae216563cb3942
Sondre Vestad <sonves@gmail.com>
2024-02-19 14:25:08 +0100

    all curators should have resource-accessright

--- user-access-handlers/src/main/java/no/unit/nva/handlers/data/DefaultRoleSource.java
+++ user-access-handlers/src/main/java/no/unit/nva/handlers/data/DefaultRoleSource.java
@@ -22,7 +22,6 @@ import no.unit.nva.useraccessservice.model.RoleDto;
 
 public class DefaultRoleSource implements RoleSource {
 
-    protected static final String CURATOR_ROLE_NAME = "Curator";
     protected static final String PUBLISHING_CURATOR_ROLE_NAME = "Publishing-Curator";
     protected static final String DOI_CURATOR_ROLE_NAME = "Doi-Curator";
     protected static final String NVI_CURATOR_ROLE_NAME = "Nvi-Curator";
@@ -39,14 +38,6 @@ public class DefaultRoleSource implements RoleSource {
                                                     .withAccessRights(List.of(MANAGE_OWN_RESOURCES))
                                                     .build();
 
-    private static final RoleDto CURATOR_ROLE = RoleDto.newBuilder()
-                                                    .withRoleName(CURATOR_ROLE_NAME)
-                                                    .withAccessRights(List.of(MANAGE_DOI,
-                                                                              MANAGE_RESOURCES_STANDARD,
-                                                                              MANAGE_PUBLISHING_REQUESTS,
-                                                                              SUPPORT
-                                                    ))
-                                                    .build();
     public static final RoleDto PUBLISHING_CURATOR_ROLE = RoleDto.newBuilder()
                                                     .withRoleName(PUBLISHING_CURATOR_ROLE_NAME)
                                                     .withAccessRights(List.of(MANAGE_RESOURCES_STANDARD,
@@ -81,13 +72,15 @@ public class DefaultRoleSource implements RoleSource {
 
     private static final RoleDto CURATOR_THESIS_ROLE = RoleDto.newBuilder()
                                                            .withRoleName(CURATOR_THESIS_ROLE_NAME)
-                                                           .withAccessRights(List.of(MANAGE_DEGREE))
+                                                           .withAccessRights(List.of(MANAGE_RESOURCES_STANDARD,
+                                                                                     MANAGE_DEGREE))
                                                            .build();
 
     private static final RoleDto CURATOR_THESIS_EMBARGO_ROLE = RoleDto.newBuilder()
                                                                    .withRoleName(CURATOR_THESIS_EMBARGO_ROLE_NAME)
                                                                    .withAccessRights(
-                                                                       List.of(MANAGE_DEGREE_EMBARGO))
+                                                                       List.of(MANAGE_RESOURCES_STANDARD,
+                                                                               MANAGE_DEGREE_EMBARGO))
                                                                    .build();
 
     private static final RoleDto APPLICATION_ADMIN_ROLE = RoleDto.newBuilder()
@@ -110,7 +103,6 @@ public class DefaultRoleSource implements RoleSource {
     @Override
     public List<RoleDto> roles() {
         return List.of(CREATOR_ROLE,
-                       CURATOR_ROLE,
                        PUBLISHING_CURATOR_ROLE,
                        NVI_CURATOR_ROLE,
                        DOI_CURATOR_ROLE,

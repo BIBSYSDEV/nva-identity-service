f63282e286339627eca392fed5dcb5f1dbbaf03d
Torbjørn Kvåle <torbjokv@users.noreply.github.com>
2024-02-27 17:59:27 +0100

    feat: get external client without passing client id in path #NP-46258 (#346)

--- user-access-handlers/src/test/java/no/unit/nva/handlers/GetExternalClientUserinfoHandlerTest.java
+++ user-access-handlers/src/test/java/no/unit/nva/handlers/GetExternalClientUserinfoHandlerTest.java
@@ -1,52 +1,41 @@
 package no.unit.nva.handlers;
 
-import static java.net.HttpURLConnection.HTTP_NOT_FOUND;
 import static java.net.HttpURLConnection.HTTP_OK;
 import static no.unit.nva.commons.json.JsonUtils.dtoObjectMapper;
-import static no.unit.nva.handlers.GetExternalClientHandler.CLIENT_ID_PATH_PARAMETER_NAME;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Map;
 import no.unit.nva.stubs.FakeContext;
 import no.unit.nva.testutils.HandlerRequestBuilder;
 import no.unit.nva.testutils.RandomDataGenerator;
 import no.unit.nva.useraccessservice.model.ClientDto;
 import no.unit.nva.useraccessservice.model.CreateExternalClientRequest;
+import no.unit.nva.useraccessservice.model.GetExternalClientResponse;
 import nva.commons.apigateway.GatewayResponse;
-import nva.commons.apigateway.RequestInfoConstants;
-import nva.commons.apigateway.exceptions.ConflictException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.zalando.problem.Problem;
 
-class GetExternalClientHandlerTest extends HandlerTest {
+public class GetExternalClientUserinfoHandlerTest extends HandlerTest {
 
     private FakeContext context;
     private ByteArrayOutputStream outputStream;
-    private GetExternalClientHandler handler;
+    private GetExternalClientUserinfoHandler handler;
 
     @BeforeEach
     public void setup() {
         databaseService = createDatabaseServiceUsingLocalStorage();
         context = new FakeContext();
         outputStream = new ByteArrayOutputStream();
-        handler = new GetExternalClientHandler(databaseService);
+        handler = new GetExternalClientUserinfoHandler(databaseService);
     }
 
     @Test
-    public void shouldReturnNotFoundWhenTheClientDoesNotExist() throws IOException {
-        var gatewayResponse = sendRequest(createBackendRequest("someClientId"), Problem.class);
-
-        assertThat(gatewayResponse.getStatusCode(), is(equalTo(HTTP_NOT_FOUND)));
-    }
-
-    @Test
-    public void shouldReturnTheClientWhenItExists() throws IOException, ConflictException {
+    public void shouldReturnTheClientWithOnlyExternalToken() throws IOException {
         var client =
             ClientDto.newBuilder()
                 .withClientId("someClientId")
@@ -56,9 +45,11 @@ class GetExternalClientHandlerTest extends HandlerTest {
                 .build();
 
         insertClientToDatabase(client);
-        var gatewayResponse = sendRequest(createBackendRequest("someClientId"), Problem.class);
+        var gatewayResponse = sendRequest(createRequestWithClientInToken("someClientId"),
+                                          GetExternalClientResponse.class);
 
         assertThat(gatewayResponse.getStatusCode(), is(equalTo(HTTP_OK)));
+        assertThat(gatewayResponse.getBody(), containsString("customerUri"));
     }
 
     private <T> GatewayResponse<T> sendRequest(InputStream request, Class<T> responseType) throws IOException {
@@ -66,13 +57,11 @@ class GetExternalClientHandlerTest extends HandlerTest {
         return GatewayResponse.fromOutputStream(outputStream, responseType);
     }
 
-    private InputStream createBackendRequest(String clientId)
+    private InputStream createRequestWithClientInToken(String clientId)
         throws JsonProcessingException {
-        var pathParams = Map.of(CLIENT_ID_PATH_PARAMETER_NAME, clientId);
 
         return new HandlerRequestBuilder<CreateExternalClientRequest>(dtoObjectMapper)
-                   .withScope(RequestInfoConstants.BACKEND_SCOPE_AS_DEFINED_IN_IDENTITY_SERVICE)
-                   .withPathParameters(pathParams)
+                   .withClientId(clientId)
                    .build();
     }
 }

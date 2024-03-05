db0d26a8ad426c5923fdf840c636efb6f0e6ad8e
Kir Truhacev <kir.truhacev@sikt.no>
2024-03-05 15:25:36 +0100

    code review with frontend + bug fix

--- user-access-handlers/src/main/java/no/unit/nva/handlers/models/CreateUserRequest.java
+++ user-access-handlers/src/main/java/no/unit/nva/handlers/models/CreateUserRequest.java
@@ -1,16 +1,12 @@
 package no.unit.nva.handlers.models;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import java.net.URI;
 import java.util.Set;
 import no.unit.nva.useraccessservice.model.RoleDto;
 import no.unit.nva.useraccessservice.model.ViewingScope;
 
-@JsonAutoDetect(fieldVisibility = Visibility.ANY)
-@JsonTypeInfo(use = Id.NAME)
+@JsonSerialize
 public record CreateUserRequest(String nationalIdentityNumber, URI customerId, Set<RoleDto> roles,
                                 ViewingScope viewingScope) {
 

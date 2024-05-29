007848af8ab5682172c914c105d0f77fc5a6163d
anetteOlli <44363838+anetteOlli@users.noreply.github.com>
2024-05-29 15:02:50 +0200

    NP-46866-security-headers (#362)
    Sett over at alle handlere som finnes i swagger spesifikasjonen arver
    ApiGatewayHandler.
    Dette api'et kjører på java 17, men ser ut til å compile uten å måtte
    oppgradere til java 21.

--- customer/src/test/java/no/unit/nva/customer/testing/TestHeaders.java
+++ customer/src/test/java/no/unit/nva/customer/testing/TestHeaders.java
@@ -3,15 +3,15 @@ package no.unit.nva.customer.testing;
 import static com.google.common.net.HttpHeaders.ACCEPT;
 import static com.google.common.net.HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN;
 import static com.google.common.net.HttpHeaders.CONTENT_TYPE;
+import static com.google.common.net.HttpHeaders.STRICT_TRANSPORT_SECURITY;
+import static com.google.common.net.HttpHeaders.X_CONTENT_TYPE_OPTIONS;
 import static com.google.common.net.MediaType.JSON_UTF_8;
-import java.util.List;
 import java.util.Map;
 import nva.commons.core.JacocoGenerated;
 
 @JacocoGenerated
 public class TestHeaders {
 
-    public static String APPLICATION_PROBLEM_JSON = "application/problem+json";
     public static String WILDCARD = "*";
 
     /**
@@ -25,12 +25,6 @@ public class TestHeaders {
             ACCEPT, JSON_UTF_8.toString());
     }
 
-    public static Map<String, List<String>> getMultiValuedHeaders() {
-        return Map.of(
-            CONTENT_TYPE, List.of(JSON_UTF_8.toString()),
-            ACCEPT, List.of(JSON_UTF_8.toString()));
-    }
-
     /**
      * Successful response headers for testing.
      *
@@ -39,19 +33,9 @@ public class TestHeaders {
     public static Map<String, String> getResponseHeaders() {
         return Map.of(
             CONTENT_TYPE, JSON_UTF_8.toString(),
-            ACCESS_CONTROL_ALLOW_ORIGIN, WILDCARD
-        );
-    }
-
-    /**
-     * Failing response headers for testing.
-     *
-     * @return headers
-     */
-    public static Map<String, String> getErrorResponseHeaders() {
-        return Map.of(
-            CONTENT_TYPE, APPLICATION_PROBLEM_JSON,
-            ACCESS_CONTROL_ALLOW_ORIGIN, WILDCARD
+            ACCESS_CONTROL_ALLOW_ORIGIN, WILDCARD,
+            STRICT_TRANSPORT_SECURITY, "max-age=63072000; includeSubDomains; preload",
+            X_CONTENT_TYPE_OPTIONS, "nosniff"
         );
     }
 }

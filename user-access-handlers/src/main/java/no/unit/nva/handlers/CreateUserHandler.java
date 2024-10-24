ad8b49ecfa58a9d1e3923f2c65d9a391b05ee352
Ketil Aasarød <ketil.aasarod@sikt.no>
2024-10-24 15:08:09 +0200

    help codacy understand this is not problem

--- user-access-handlers/src/main/java/no/unit/nva/handlers/CreateUserHandler.java
+++ user-access-handlers/src/main/java/no/unit/nva/handlers/CreateUserHandler.java
@@ -232,7 +232,8 @@ public class CreateUserHandler extends HandlerWithEventualConsistency<CreateUser
         if (exception instanceof ConflictException) {
             return (ConflictException) exception;
         } else {
-            LOGGER.error("Failed to create or update user!", failure.getException());
+            final Throwable cause = failure.getException();
+            LOGGER.error("Failed to create or update user!", cause);
             return new BadGatewayException(BAD_GATEWAY_ERROR_MESSAGE);
         }
     }

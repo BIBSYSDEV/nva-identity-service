24f640943bcc753e8f7636ad334d7844121dad58
Torbjørn Kvåle <torbjorn.kvale@sikt.no>
2025-03-21 13:16:54 +0100

    refactor(CustomerSelectionHandler): upgrade nva-commons to v2-0-0 and remove full user attributes update from CustomerSelectionHandler #NP-48657

--- cognito-pre-token-generation-openid/src/test/java/no/unit/nva/cognito/CustomerSelectionHandlerTest.java
+++ cognito-pre-token-generation-openid/src/test/java/no/unit/nva/cognito/CustomerSelectionHandlerTest.java
@@ -1,5 +1,7 @@
 package no.unit.nva.cognito;
 
+import com.amazonaws.services.lambda.runtime.CognitoIdentity;
+import com.amazonaws.services.lambda.runtime.Context;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import no.unit.nva.FakeCognito;
 import no.unit.nva.customer.model.ApplicationDomain;
@@ -10,20 +12,16 @@ import no.unit.nva.customer.testing.LocalCustomerServiceDatabase;
 import no.unit.nva.database.IdentityService;
 import no.unit.nva.database.IdentityServiceImpl;
 import no.unit.nva.database.LocalIdentityService;
-import no.unit.nva.database.TermsAndConditionsService;
-import no.unit.nva.stubs.FakeContext;
 import no.unit.nva.testutils.HandlerRequestBuilder;
 import no.unit.nva.testutils.JwtTestToken;
 import no.unit.nva.useraccessservice.dao.RoleDb;
 import no.unit.nva.useraccessservice.exceptions.InvalidInputException;
 import no.unit.nva.useraccessservice.model.CustomerSelection;
 import no.unit.nva.useraccessservice.model.RoleDto;
-import no.unit.nva.useraccessservice.model.TermsConditionsResponse;
 import no.unit.nva.useraccessservice.model.UserDto;
 import nva.commons.apigateway.AccessRight;
 import nva.commons.apigateway.GatewayResponse;
 import nva.commons.apigateway.exceptions.ConflictException;
-import nva.commons.apigateway.exceptions.NotFoundException;
 import nva.commons.core.SingletonCollector;
 import nva.commons.core.attempt.Try;
 import nva.commons.core.paths.UriWrapper;
@@ -39,7 +37,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URI;
-import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -47,17 +44,11 @@ import java.util.stream.IntStream;
 import software.amazon.awssdk.services.cognitoidentityprovider.model.UpdateUserAttributesRequest;
 
 import static no.unit.nva.RandomUserDataGenerator.randomRoleName;
-import static no.unit.nva.cognito.CognitoClaims.ACCESS_RIGHTS_CLAIM;
 import static no.unit.nva.cognito.CognitoClaims.ALLOWED_CUSTOMERS_CLAIM;
 import static no.unit.nva.cognito.CognitoClaims.CURRENT_CUSTOMER_CLAIM;
 import static no.unit.nva.cognito.CognitoClaims.CURRENT_TERMS;
 import static no.unit.nva.cognito.CognitoClaims.CUSTOMER_ACCEPTED_TERMS;
-import static no.unit.nva.cognito.CognitoClaims.NVA_USERNAME_CLAIM;
-import static no.unit.nva.cognito.CognitoClaims.PERSON_AFFILIATION_CLAIM;
 import static no.unit.nva.cognito.CognitoClaims.PERSON_ID_CLAIM;
-import static no.unit.nva.cognito.CognitoClaims.ROLES_CLAIM;
-import static no.unit.nva.cognito.CognitoClaims.TOP_ORG_CRISTIN_ID;
-import static no.unit.nva.cognito.CustomerSelectionHandler.AUTHORIZATION_HEADER;
 import static no.unit.nva.commons.json.JsonUtils.dtoObjectMapper;
 import static no.unit.nva.testutils.RandomDataGenerator.randomElement;
 import static no.unit.nva.testutils.RandomDataGenerator.randomString;
@@ -69,17 +60,13 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.collection.IsIn.in;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
-import static org.hamcrest.core.IsNot.not;
-import static org.hamcrest.core.IsNull.nullValue;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 class CustomerSelectionHandlerTest {
 
     public static final String MULTI_VALUE_CLAIMS_DELIMITER = ",";
-    static final URI TERMS_URL = URI.create("https://nva.sikt.no/terms/2024-10-01");
-    private final FakeContext context = new FakeContext();
+    private final Context context = mock(Context.class);
     private FakeCognito cognito;
     private String personAccessToken;
     private Set<URI> allowedCustomers;
@@ -91,7 +78,6 @@ class CustomerSelectionHandlerTest {
     private ByteArrayOutputStream outputStream;
     private AccessRight accessRight;
     private RoleDto role;
-    private TermsAndConditionsService termsService;
 
     @BeforeEach
     public void init() throws InvalidInputException, ConflictException {
@@ -104,16 +90,10 @@ class CustomerSelectionHandlerTest {
         personAccessToken = JwtTestToken.randomToken();
         var person = randomUri();
         setupCognitoAndPersonInformation(person);
-        termsService = mock(TermsAndConditionsService.class);
-        var terms = TermsConditionsResponse.builder()
-                                            .withTermsConditionsUri(TERMS_URL)
-                                            .build();
-        when(termsService.getCurrentTermsAndConditions()).thenReturn(terms);
-        when(termsService.getTermsAndConditionsByPerson(any())).thenReturn(terms);
 
         addUserEntriesInIdentityService(person, allowedCustomers, role);
 
-        this.handler = new CustomerSelectionHandler(cognito, customerService, identityService, termsService);
+        this.handler = new CustomerSelectionHandler(cognito);
     }
 
     private static RoleDb randomRoleWithAccessRight(AccessRight accessRight) {
@@ -249,10 +229,10 @@ class CustomerSelectionHandlerTest {
         var selectedCustomer = randomElement(allowedCustomers.toArray(URI[]::new));
         var input = createRequest(selectedCustomer);
         var response = sendRequest(input, Void.class);
-        var updatedSelectedCustomer = extractAttributeUpdate(CURRENT_CUSTOMER_CLAIM);
+        var updatedSelectedCustomer = extractSelectedCustomerAttributeUpdate();
 
         assertThat(response.getStatusCode(), is(equalTo(HttpURLConnection.HTTP_OK)));
-        assertThat(URI.create(updatedSelectedCustomer), is(in(allowedCustomers.toArray(URI[]::new))));
+        assertThat(updatedSelectedCustomer, is(in(allowedCustomers.toArray(URI[]::new))));
     }
 
     private <T> GatewayResponse<T> sendRequest(InputStream input, Class<T> responseType) throws IOException {
@@ -260,117 +240,38 @@ class CustomerSelectionHandlerTest {
         return GatewayResponse.fromOutputStream(outputStream, responseType);
     }
 
-    private String extractAttributeUpdate(String attributeName) {
-        var request = cognito.getUpdateUserAttributesRequest();
+    private URI extractSelectedCustomerAttributeUpdate() {
+
+        var request = cognito.getAdminUpdateUserRequest();
         return request
                    .userAttributes()
                    .stream()
-                   .filter(attribute -> attributeName.equals(attribute.name()))
+                   .filter(attribute -> CURRENT_CUSTOMER_CLAIM.equals(attribute.name()))
                    .map(AttributeType::value)
-                   .collect(SingletonCollector.collect());
+                   .map(URI::create).findFirst().orElseThrow();
     }
 
     private InputStream createRequest(URI customerId) throws JsonProcessingException {
         var randomCustomer = CustomerSelection.fromCustomerId(customerId);
+        when(context.getIdentity()).thenReturn(new CognitoIdentity() {
+            @Override
+            public String getIdentityId() {
+                return randomString();
+            }
+
+            @Override
+            public String getIdentityPoolId() {
+                return randomString();
+            }
+        });
+
         return new HandlerRequestBuilder<CustomerSelection>(dtoObjectMapper)
-                   .withHeaders(Map.of(AUTHORIZATION_HEADER, bearerToken()))
                    .withPersonCristinId(randomUri())
-                   .withCurrentCustomer(randomUri())
+                   .withCurrentCustomer(customerId)
+                   .withAllowedCustomers(allowedCustomers)
                    .withUserName(randomString())
                    .withBody(randomCustomer)
                    .build();
     }
 
-    private String bearerToken() {
-        return "Bearer " + personAccessToken;
-    }
-
-    @Test
-    void shouldSendAnUpdateAccessRightRequestToCognitoWhenInputContainsAnAccessTokenAndSelectionIsAmongTheValidOptions()
-        throws IOException {
-        var selectedCustomer = randomElement(allowedCustomers.toArray(URI[]::new));
-        var input = createRequest(selectedCustomer);
-        var response = sendRequest(input, Void.class);
-        var updatedAccessRights = extractAttributeUpdate(ACCESS_RIGHTS_CLAIM);
-        var expectedAccessRights = accessRight.toPersistedString();
-
-        assertThat(response.getStatusCode(), is(equalTo(HttpURLConnection.HTTP_OK)));
-        assertThat(updatedAccessRights, is(equalTo(expectedAccessRights)));
-    }
-
-    @Test
-    void shouldUpdateRoleInCognitoUserEntry()
-        throws IOException {
-        var selectedCustomer = randomElement(allowedCustomers.toArray(URI[]::new));
-        var input = createRequest(selectedCustomer);
-        var response = sendRequest(input, Void.class);
-        var updatedRoles = extractAttributeUpdate(ROLES_CLAIM);
-        var expectedRoles = role.getRoleName();
-        assertThat(updatedRoles, is(equalTo(expectedRoles.getValue())));
-        assertThat(response.getStatusCode(), is(equalTo(HttpURLConnection.HTTP_OK)));
-    }
-
-    @Test
-    void shouldUpdateNvaUsernameInCognitoUserEntry() throws IOException, NotFoundException {
-        var selectedCustomer = randomElement(allowedCustomers.toArray(URI[]::new));
-        var input = createRequest(selectedCustomer);
-        var response = sendRequest(input, Void.class);
-        var userForSelectedCustomer = extractAttributeUpdate(NVA_USERNAME_CLAIM);
-        var expectedUsername = constructExpectedUserName(selectedCustomer);
-        assertThat(userForSelectedCustomer, is(equalTo(expectedUsername)));
-        assertThat(response.getStatusCode(), is(equalTo(HttpURLConnection.HTTP_OK)));
-    }
-
-    private String constructExpectedUserName(URI selectedCustomer) throws NotFoundException {
-        var personId = URI.create(extractPersonIdFromCognitoData());
-        var customerCristinId = customerService.getCustomer(selectedCustomer).getCristinId();
-        var user = identityService.getUserByPersonCristinIdAndCustomerCristinId(personId, customerCristinId);
-        return user.getUsername();
-    }
-
-    @Test
-    void shouldNotSendAnUpdateCustomerRequestToCognitoWhenInputDoesNotContainAccessToken()
-        throws IOException {
-        cognito.updateUserAttributes((UpdateUserAttributesRequest)null);//clear mock value@
-        var input = createRequest(randomUri());
-        var response = sendRequest(input, Void.class);
-        assertThatUpdateRequestHasNotBeenSent();
-        assertThat(response.getStatusCode(), is(equalTo(HttpURLConnection.HTTP_FORBIDDEN)));
-    }
-
-    private void assertThatUpdateRequestHasNotBeenSent() {
-        assertThat(cognito.getUpdateUserAttributesRequest(), is(nullValue()));
-    }
-
-    @Test
-    void shouldNotUseAdminRightsButOnlyAccessTokenBasedAccessToCognito() throws IOException {
-        var selectedCustomer = randomElement(allowedCustomers.toArray(URI[]::new));
-        var input = createRequest(selectedCustomer);
-        sendRequest(input, Void.class);
-        assertThat(cognito.getAdminUpdateUserRequest(), is(nullValue()));
-        assertThat(cognito.getUpdateUserAttributesRequest(), is(not(nullValue())));
-    }
-
-    @Test
-    void shouldUpdateCustomerCristinWhenSelectingCustomer() throws IOException, NotFoundException {
-        var selectedCustomer = randomElement(allowedCustomers.toArray(URI[]::new));
-        var input = createRequest(selectedCustomer);
-        sendRequest(input, Void.class);
-        var cristinIdForSelectedCustomer = extractAttributeUpdate(TOP_ORG_CRISTIN_ID);
-        var expectedCristinId = customerService.getCustomer(selectedCustomer).getCristinId();
-        assertThat(cristinIdForSelectedCustomer, is(equalTo(expectedCristinId.toString())));
-    }
-
-    @Test
-    void shouldUpdatePersonAffiliationWhenSelectingCustomer() throws IOException, NotFoundException {
-        var selectedCustomer = randomElement(allowedCustomers.toArray(URI[]::new));
-        var input = createRequest(selectedCustomer);
-        handler.handleRequest(input, outputStream, context);
-        var cristinPersonId = URI.create(extractPersonIdFromCognitoData());
-        var customerCristinId = customerService.getCustomer(selectedCustomer).getCristinId();
-        var user = identityService.getUserByPersonCristinIdAndCustomerCristinId(cristinPersonId, customerCristinId);
-        var expectedPersonAffiliation = user.getAffiliation();
-        var actualPersonAffiliation = extractAttributeUpdate(PERSON_AFFILIATION_CLAIM);
-        assertThat(actualPersonAffiliation, is(equalTo(expectedPersonAffiliation.toString())));
-    }
 }

b3654fc0235fd862065cf573e10f71ac8caec273
Joachim Jørgensen <joachim.jorgensen@Joachims-MacBook-Pro.local>
2025-04-14 13:28:28 +0200

    NP-48856 Deny trailing year in channel claim

--- customer-commons/src/main/java/no/unit/nva/customer/validator/ChannelClaimValidator.java
+++ customer-commons/src/main/java/no/unit/nva/customer/validator/ChannelClaimValidator.java
@@ -1,19 +1,24 @@
 package no.unit.nva.customer.validator;
 
 import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
 import java.net.URI;
 import no.unit.nva.customer.model.channelclaim.ChannelClaimDto;
 import nva.commons.apigateway.exceptions.BadRequestException;
 import nva.commons.core.Environment;
 import nva.commons.core.JacocoGenerated;
+import nva.commons.core.paths.UriWrapper;
 
 public final class ChannelClaimValidator {
+
     private static final String API_HOST = "API_HOST";
     private static final String CHANNEL_REQUIRED = "Channel required";
     private static final String INVALID_CHANNEL_MESSAGE = "Invalid channel";
     private static final Environment ENVIRONMENT = new Environment();
     private static final String PUBLICATION_CHANNEL_PATH = ENVIRONMENT.readEnv("PUBLICATION_CHANNEL_PATH");
     private static final String HOST = ENVIRONMENT.readEnv(API_HOST);
+    private static final String TRAILING_YEAR_MESSAGE = "Channel should not have trailing year";
+    private static final String REGEX_FOUR_DIGITS = "\\d{4}";
 
     @JacocoGenerated
     public ChannelClaimValidator() {}
@@ -25,9 +30,17 @@ public final class ChannelClaimValidator {
         if (isNotPublicationChannel(channelClaim.channel())) {
             throw new BadRequestException(INVALID_CHANNEL_MESSAGE);
         }
+        if (hasTrailingYear(channelClaim.channel())) {
+            throw new BadRequestException(TRAILING_YEAR_MESSAGE);
+        }
     }
 
     private static boolean isNotPublicationChannel(URI channel) {
         return !HOST.equals(channel.getHost()) || !channel.toString().contains(PUBLICATION_CHANNEL_PATH);
+    }
+
+    private static boolean hasTrailingYear(URI channel) {
+        var lastPathElement = UriWrapper.fromUri(channel).getLastPathElement();
+        return nonNull(lastPathElement) && lastPathElement.matches(REGEX_FOUR_DIGITS);
     }
 }
--- customer-commons/src/test/java/no/unit/nva/customer/validator/ChannelClaimValidatorTest.java
+++ customer-commons/src/test/java/no/unit/nva/customer/validator/ChannelClaimValidatorTest.java
@@ -1,13 +1,26 @@
 package no.unit.nva.customer.validator;
 
+import static no.unit.nva.customer.testing.CustomerDataGenerator.randomChannel;
+import static no.unit.nva.customer.testing.CustomerDataGenerator.randomChannelClaimDto;
 import static no.unit.nva.customer.testing.CustomerDataGenerator.randomChannelConstraintDto;
 import static no.unit.nva.testutils.RandomDataGenerator.randomUri;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import no.unit.nva.customer.model.channelclaim.ChannelClaimDto;
 import nva.commons.apigateway.exceptions.BadRequestException;
+import nva.commons.core.paths.UriWrapper;
 import org.junit.jupiter.api.Test;
 
 class ChannelClaimValidatorTest {
+
+    private static final String RANDOM_YEAR = "2025";
+
+    @Test
+    void shouldNotThrowExceptionWhenChannelClaimIsValid() {
+        var validChannelClaim = randomChannelClaimDto();
+        assertDoesNotThrow(() -> ChannelClaimValidator.validate(validChannelClaim));
+    }
+
     @Test
     void shouldThrowBadRequestExceptionWhenChannelIsMissing() {
         var channelClaimWithoutChannel = new ChannelClaimDto(null, randomChannelConstraintDto());
@@ -17,6 +30,13 @@ class ChannelClaimValidatorTest {
     @Test
     void shouldThrowBadRequestExceptionWhenChannelIsInvalid() {
         var channelClaimWithInvalidChannel = new ChannelClaimDto(randomUri(), randomChannelConstraintDto());
+        assertThrows(BadRequestException.class, () -> ChannelClaimValidator.validate(channelClaimWithInvalidChannel));
+    }
+
+    @Test
+    void shouldThrowBadRequestExceptionWhenChannelHasTrailingYear() {
+        var channelWithTrailingYear = UriWrapper.fromUri(randomChannel()).addChild(RANDOM_YEAR).getUri();
+        var channelClaimWithInvalidChannel = new ChannelClaimDto(channelWithTrailingYear, randomChannelConstraintDto());
         assertThrows(BadRequestException.class, () -> ChannelClaimValidator.validate(channelClaimWithInvalidChannel));
     }
 }

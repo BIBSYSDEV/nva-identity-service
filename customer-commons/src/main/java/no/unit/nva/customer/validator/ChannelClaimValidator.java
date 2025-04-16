fdc76912ed387024b23e0780f7d7bea733ab4a79
Joachim Jørgensen <joachim.jorgensen@sikt.no>
2025-04-16 09:25:33 +0200

    NP-48856 Deny trailing year in channel claim (#434)
    * NP-48856 Deny trailing year in channel claim
    
    * Check that a channel is valid, instead of specific invalidity
    
    * Feedback QA -> more clean uuid and null check
    
    ---------
    
    Co-authored-by: Joachim Jørgensen <joachim.jorgensen@Joachims-MacBook-Pro.local>

--- customer-commons/src/main/java/no/unit/nva/customer/validator/ChannelClaimValidator.java
+++ customer-commons/src/main/java/no/unit/nva/customer/validator/ChannelClaimValidator.java
@@ -1,33 +1,65 @@
 package no.unit.nva.customer.validator;
 
-import static java.util.Objects.isNull;
+import static nva.commons.core.attempt.Try.attempt;
 import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import no.unit.nva.customer.model.channelclaim.ChannelClaimDto;
 import nva.commons.apigateway.exceptions.BadRequestException;
 import nva.commons.core.Environment;
 import nva.commons.core.JacocoGenerated;
 
 public final class ChannelClaimValidator {
+
     private static final String API_HOST = "API_HOST";
     private static final String CHANNEL_REQUIRED = "Channel required";
     private static final String INVALID_CHANNEL_MESSAGE = "Invalid channel";
     private static final Environment ENVIRONMENT = new Environment();
     private static final String PUBLICATION_CHANNEL_PATH = ENVIRONMENT.readEnv("PUBLICATION_CHANNEL_PATH");
     private static final String HOST = ENVIRONMENT.readEnv(API_HOST);
+    private static final String SERIAL_PUBLICATION = "serial-publication";
+    private static final String PUBLISHER = "publisher";
+    private static final List<String> CHANNEL_TYPES = List.of(SERIAL_PUBLICATION, PUBLISHER);
+    private static final String SLASH = "/";
+    private static final String EMPTY_STRING = "";
+    private static final int THREE = 3;
 
     @JacocoGenerated
     public ChannelClaimValidator() {}
 
     public static void validate(ChannelClaimDto channelClaim) throws BadRequestException {
-        if (isNull(channelClaim) || isNull(channelClaim.channel())) {
+        if (Optional.ofNullable(channelClaim).map(ChannelClaimDto::channel).isEmpty()) {
             throw new BadRequestException(CHANNEL_REQUIRED);
         }
-        if (isNotPublicationChannel(channelClaim.channel())) {
+        if (!isValidPublicationChannel(channelClaim.channel())) {
             throw new BadRequestException(INVALID_CHANNEL_MESSAGE);
         }
     }
 
-    private static boolean isNotPublicationChannel(URI channel) {
-        return !HOST.equals(channel.getHost()) || !channel.toString().contains(PUBLICATION_CHANNEL_PATH);
+    private static boolean isValidPublicationChannel(URI channel) {
+        return HOST.equals(channel.getHost()) && pathIsValid(channel);
+    }
+
+    private static boolean pathIsValid(URI channel) {
+        var pathElements = channel.getPath().replaceFirst(SLASH, EMPTY_STRING).split(SLASH);
+
+        if (pathElements.length != THREE) {
+            return false;
+        }
+
+        return isChannelPath(pathElements[0]) && isChannelType(pathElements[1]) && isUuid(pathElements[2]);
+    }
+
+    private static boolean isChannelPath(String string) {
+        return PUBLICATION_CHANNEL_PATH.equals(string);
+    }
+
+    private static boolean isChannelType(String string) {
+        return CHANNEL_TYPES.contains(string);
+    }
+
+    private static boolean isUuid(String string) {
+        return attempt(() -> UUID.fromString(string)).isSuccess();
     }
 }
--- customer-commons/src/test/java/no/unit/nva/customer/validator/ChannelClaimValidatorTest.java
+++ customer-commons/src/test/java/no/unit/nva/customer/validator/ChannelClaimValidatorTest.java
@@ -1,13 +1,28 @@
 package no.unit.nva.customer.validator;
 
+import static no.unit.nva.customer.testing.CustomerDataGenerator.randomChannel;
+import static no.unit.nva.customer.testing.CustomerDataGenerator.randomChannelClaimDto;
 import static no.unit.nva.customer.testing.CustomerDataGenerator.randomChannelConstraintDto;
 import static no.unit.nva.testutils.RandomDataGenerator.randomUri;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import no.unit.nva.customer.model.channelclaim.ChannelClaimDto;
 import nva.commons.apigateway.exceptions.BadRequestException;
+import nva.commons.core.paths.UriWrapper;
 import org.junit.jupiter.api.Test;
 
 class ChannelClaimValidatorTest {
+
+    private static final String RANDOM_YEAR = "2025";
+    private static final int ZERO = 0;
+    private static final String EMPTY_STRING = "";
+
+    @Test
+    void shouldNotThrowExceptionWhenChannelClaimIsValid() {
+        var validChannelClaim = randomChannelClaimDto();
+        assertDoesNotThrow(() -> ChannelClaimValidator.validate(validChannelClaim));
+    }
+
     @Test
     void shouldThrowBadRequestExceptionWhenChannelIsMissing() {
         var channelClaimWithoutChannel = new ChannelClaimDto(null, randomChannelConstraintDto());
@@ -17,6 +32,22 @@ class ChannelClaimValidatorTest {
     @Test
     void shouldThrowBadRequestExceptionWhenChannelIsInvalid() {
         var channelClaimWithInvalidChannel = new ChannelClaimDto(randomUri(), randomChannelConstraintDto());
+        assertThrows(BadRequestException.class, () -> ChannelClaimValidator.validate(channelClaimWithInvalidChannel));
+    }
+
+    @Test
+    void shouldThrowBadRequestExceptionWhenChannelHasTrailingYear() {
+        var channelWithTrailingYear = UriWrapper.fromUri(randomChannel()).addChild(RANDOM_YEAR).getUri();
+        var channelClaimWithInvalidChannel = new ChannelClaimDto(channelWithTrailingYear, randomChannelConstraintDto());
+        assertThrows(BadRequestException.class, () -> ChannelClaimValidator.validate(channelClaimWithInvalidChannel));
+    }
+
+    @Test
+    void shouldThrowBadRequestExceptionWhenLastPathElementOfChannelIsNotUuid() {
+        var channelWithRandomLastPathElement = UriWrapper.fromUri(randomChannel())
+                                                   .replacePathElementByIndexFromEnd(ZERO, EMPTY_STRING)
+                                                   .getUri();
+        var channelClaimWithInvalidChannel = new ChannelClaimDto(channelWithRandomLastPathElement, randomChannelConstraintDto());
         assertThrows(BadRequestException.class, () -> ChannelClaimValidator.validate(channelClaimWithInvalidChannel));
     }
 }

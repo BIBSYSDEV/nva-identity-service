95cda8e45afb681740f654984443c531d4909f6a
Joachim Jørgensen <joachim.jorgensen@Joachims-MacBook-Pro.local>
2025-04-23 11:20:20 +0200

    Add type to ChannelClaimConstraint and fix typo in swagger

--- customer-commons/src/main/java/no/unit/nva/customer/model/channelclaim/ChannelConstraintDto.java
+++ customer-commons/src/main/java/no/unit/nva/customer/model/channelclaim/ChannelConstraintDto.java
@@ -1,10 +1,14 @@
 package no.unit.nva.customer.model.channelclaim;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import no.unit.nva.commons.json.JsonSerializable;
 
 import java.util.List;
 import no.unit.nva.customer.model.PublicationInstanceTypes;
 
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
+@JsonTypeName("ChannelConstraint")
 public record ChannelConstraintDto(ChannelConstraintPolicy publishingPolicy,
                                    ChannelConstraintPolicy editingPolicy,
                                    List<PublicationInstanceTypes> scope) implements JsonSerializable {

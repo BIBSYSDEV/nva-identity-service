8793ee69fd6e3d68f260da4fe3ae8705bc7b5831
Joachim Jørgensen <joachim.jorgensen@sikt.no>
2025-04-28 15:07:01 +0200

    Tighten is channel type check getter (#443)
    * Tighten isChannelType check when filtering fetched claims
    
    * Avoid NPE when checking channel type
    
    ---------
    
    Co-authored-by: Joachim Jørgensen <joachim.jorgensen@Joachims-MacBook-Pro.local>

--- customer/src/main/java/no/unit/nva/customer/get/ListAllChannelClaimsHandler.java
+++ customer/src/main/java/no/unit/nva/customer/get/ListAllChannelClaimsHandler.java
@@ -10,6 +10,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import no.unit.nva.customer.get.response.ChannelClaimsListResponse;
+import no.unit.nva.customer.model.channelclaim.ChannelClaimDto;
 import no.unit.nva.customer.model.channelclaim.ChannelClaimWithClaimer;
 import no.unit.nva.customer.service.CustomerService;
 import nva.commons.apigateway.ApiGatewayHandler;
@@ -19,12 +20,14 @@ import nva.commons.apigateway.exceptions.BadGatewayException;
 import nva.commons.apigateway.exceptions.UnauthorizedException;
 import nva.commons.core.Environment;
 import nva.commons.core.JacocoGenerated;
+import nva.commons.core.paths.UriWrapper;
 
 public class ListAllChannelClaimsHandler extends ApiGatewayHandler<Void, ChannelClaimsListResponse> {
 
     private static final String BAD_GATEWAY_ERROR_MESSAGE = "Something went wrong, contact application administrator!";
     private static final String QUERY_PARAM_INSTITUTION = "institution";
     private static final String QUERY_PARAM_CHANNEL_TYPE = "channelType";
+    private static final int ONE = 1;
     private final CustomerService customerService;
 
     @JacocoGenerated
@@ -91,8 +94,14 @@ public class ListAllChannelClaimsHandler extends ApiGatewayHandler<Void, Channel
     }
 
     private boolean isChannelType(ChannelClaimWithClaimer claim, String channelType) {
-        return Optional.of(claim.channelClaim().channel())
-                   .map(channel -> channel.toString().contains(channelType))
-                   .orElse(false);
+        return Optional.ofNullable(claim.channelClaim())
+                   .map(ChannelClaimDto::channel)
+                   .map(this::getChannelTypeFromPath)
+                   .filter(channelTypeFromPath -> channelTypeFromPath.equals(channelType))
+                   .isPresent();
+    }
+
+    private String getChannelTypeFromPath(URI channel) {
+        return UriWrapper.fromUri(channel).getPath().getPathElementByIndexFromEnd(ONE);
     }
 }

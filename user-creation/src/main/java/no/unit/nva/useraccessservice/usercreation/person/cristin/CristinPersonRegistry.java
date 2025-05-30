5413424a2bd27adb91ef08eff389cfda29a3a90e
Torbjørn Kvåle <torbjorn.kvale@sikt.no>
2025-05-30 09:57:26 +0200

    content type

--- user-creation/src/main/java/no/unit/nva/useraccessservice/usercreation/person/cristin/CristinPersonRegistry.java
+++ user-creation/src/main/java/no/unit/nva/useraccessservice/usercreation/person/cristin/CristinPersonRegistry.java
@@ -60,6 +60,8 @@ public final class CristinPersonRegistry implements PersonRegistry {
     private static final String ORGANIZATION_PATH = "organization";
     private static final String NATIONAL_IDENTITY_PATTERN = ".*\\?national_id=(\\d+)\\d\\d$";
     private static final String PERSONS_PATH = "persons";
+    private static final String CONTENT_TYPE = "Content-Type";
+    private static final String APPLICATION_JSON = "application/json";
     private final HttpClient httpClient;
     private final URI cristinBaseUri;
     private final String apiDomain;
@@ -202,6 +204,7 @@ public final class CristinPersonRegistry implements PersonRegistry {
     private HttpRequest createPostRequest(URI uri, String body, CristinCredentials cristinCredentials) {
         var requestBuilder = HttpRequest.newBuilder(uri)
                                  .POST(HttpRequest.BodyPublishers.ofString(body))
+                                 .header(CONTENT_TYPE, APPLICATION_JSON)
                                  .header(AUTHORIZATION, generateBasicAuthorization(cristinCredentials));
 
         defaultRequestHeaders.stream()
@@ -272,6 +275,7 @@ public final class CristinPersonRegistry implements PersonRegistry {
         return attempt(() -> executeRequest(request, CristinPerson.class)).toOptional();
     }
 
+    @JacocoGenerated
     private static void printHttpRequest(HttpRequest request) {
         System.out.println("HTTP Request:");
         System.out.println("Method: " + request.method());

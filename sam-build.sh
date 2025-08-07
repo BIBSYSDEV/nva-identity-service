bf0a4f5c7b8735d8c2cb7dc33a8c9b658f352a32
Lars-Olav Vågene <42863501+LarsV123@users.noreply.github.com>
2025-08-07 13:11:33 +0200

    ci: Disable Gradle configuration cache for sam build

--- sam-build.sh
+++ sam-build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# SAM build wrapper that disables Gradle configuration cache
+# Wrapper script for running `sam build` locally
 export GRADLE_OPTS="-Dorg.gradle.configuration-cache=false"
 sam build "$@"

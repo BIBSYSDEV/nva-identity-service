c0487a71af38665a88ba2bb535777056cb1d12c3
Lars-Olav Vågene <42863501+LarsV123@users.noreply.github.com>
2026-06-08 14:03:15 +0200

    refactor: Replace star import with explicit static imports

--- user-access-handlers/src/main/java/no/unit/nva/handlers/CreateUserHandler.java
+++ user-access-handlers/src/main/java/no/unit/nva/handlers/CreateUserHandler.java
@@ -28,7 +28,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.net.URI;
-import java.util.*;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
